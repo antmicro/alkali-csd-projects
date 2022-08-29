a371910959781fa56e29e896baf20f041934ffb8
Robert Winkler <rwinkler@antmicro.com>
2022-08-29 16:26:30 +0200

    examples: Add tflite model of ADD operation
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

---
+++ examples/add/bpf.c
@@ -0,0 +1,26 @@
+static void (*print)(char*) = (void *)1;
+static void (*tflite_apu)(char*, char*, int, int, int) = (void *)2;
+static void (*tflite_vta)(char*, char*, int, int, int) = (void *)3;
+
+int bpf_prog(char *imem, char *omem)
+{
+	const int model_size = 1024;
+	const int input_size = 8;
+	const int output_size = 4;
+	char expected_output[] = {0x7, 0xC, 0x4, 0x5};
+
+	char msg[] = "VTA test of ADD operation\n";
+	print(msg);
+
+	tflite_vta(imem, omem, input_size, output_size, model_size);
+
+	for (int i = 0; i < output_size; i++) {
+		if (omem[i] != expected_output[i]) {
+			print("ADD test failed\n");
+			return i;
+		}
+	}
+
+	print("ADD test passed\n");
+	return -1;
+}
